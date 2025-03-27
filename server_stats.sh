adaaea51adee8d1563e790bfeee3bd85ac124e9d
MladenDzida <mladendzida18@gmail.com>
2025-03-27 21:11:50 +0100

    add used/free memory usage info

--- server_stats.sh
+++ server_stats.sh
@@ -2,3 +2,5 @@
 
 cpu_usage=$(top -bn1 | grep "%Cpu(s)" | sed -E 's/.*, *([0-9.]+) id.*/\1/' | awk '{print 100 - $1"%"}')
 echo "CPU Usage: $cpu_usage"
+
+free | grep Mem | awk '{print "used memory pct: "$3/$2*100.0"%" ", free memory pct: "$4/$2*100.0"%"}'
