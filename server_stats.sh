9f66d26dd053b0bacb91505923d5a5c45eb60ee0
MladenDzida <mladendzida18@gmail.com>
2025-03-29 13:24:13 +0100

    add used/free disk percentage; add top 5 Cpu/Mem processes; add os version; add uptime; add logged in users

--- server_stats.sh
+++ server_stats.sh
@@ -3,4 +3,18 @@
 cpu_usage=$(top -bn1 | grep "%Cpu(s)" | sed -E 's/.*, *([0-9.]+) id.*/\1/' | awk '{print 100 - $1"%"}')
 echo "CPU Usage: $cpu_usage"
 
-free | grep Mem | awk '{print "used memory pct: "$3/$2*100.0"%" ", free memory pct: "$4/$2*100.0"%"}'
+free | grep Mem | awk '{print "Used Memory Pct: "$3/$2*100.0"%" ", Free Memory Pct: "$4/$2*100.0"%"}'
+
+df -h / | grep / | awk '{print "Used Disk Pct: "$3/$2*100.0"%" ", Free Disk Pct: "$4/$2*100.0"%"}'
+
+echo "Top 5 Processes by Cpu Usage:"
+ps -Ao pid,comm,%cpu --sort=-%cpu | head -n 6
+
+echo "Top 5 Processes by Mem Usage:"
+ps -Ao pid,comm,%mem --sort=-%mem | head -n 6
+
+echo -n "OS Version: " && cat /etc/os-release | grep VERSION= | cut -d = -f 2 | tr -d '"'
+
+echo -n "Server Uptime: " && uptime -p
+
+echo "Logged in users:" && who
